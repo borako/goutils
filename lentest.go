0a4a28aae1c8c156f9fb3a0d26a9dd498472f0a1
Bora <pollocksoft@gmail.com>
2018-01-24 16:09:09 -0700

    a

--- lentest.go
+++ lentest.go
@@ -10,6 +10,12 @@ func main() {
   fmt.Println("Length of ", t, ": " , len(t))
   t = "테스트"
   fmt.Println("Length of ", t, ": " , len(t))
-  t = "큰테스트"
+  t = "큰 테 스 트"
   fmt.Println("Length of ", t, ": " , len(t))
+  t = "원펀맨 리메이크 127화 _ 원펀맨 (리메이크) _ 미니툰 - 만화는 언제나 미니툰 ^._.html"
+  fmt.Println("Length of ", t, ": " , len(t))
+  for pos, _ := range t {
+    fmt.Println("Len: ", pos, ": ", t[:pos])
+  }
+  fmt.Println(t[:3])
 }
